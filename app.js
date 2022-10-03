c6aa9d2721acc1441c0e17354b7da39313577c21
Sergio Baldomero Beltran Santiago <sergiobaldomerobeltran@gmail.com>
2022-10-02 18:55:57 -0700

    añadidos mas modulos de funcion

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const sequelize = require("./Database/db");
 const cors = require("cors");
 
 const Usuario = require("./Database/Models/Usuario");
+const Estudiante = require("./Database/Models/Estudiante");
+const Tramite = require("./Database/Models/Tramite");
+const Imagen = require("./Database/Models/Imagen");
+const Tramite_M = require("./Database/Models/Tramite_M");
+const Solicitud = require("./Database/Models/Solicitud");
+const Documento = require("./Database/Models/Documento");
+const Solicitud_Bitacora = require("./Database/Models/Solicitud_Bitacora");
 
 //Definimos el puerto a utilizar
 const PORT = process.env.PORT || 3001;
@@ -59,6 +66,42 @@ app.post("/AdminInfo", function (req, res) {
   });
 });
 
+//Conseguir datos del usuario activo en sesión, si es estudiante
+app.post("/StudentInfo", function (req, res) {
+  Estudiante.findAll({
+    attributes: ["matricula_E", "carrera", "semestre"],
+    where: { matricula: req.body.loginID },
+    include: [{ model: Usuario, attributes: ["nombre_C", "correo_e"] }],
+  }).then((consult) => {
+    res.send(consult);
+  });
+});
+
+//Conseguir la lista de Solicitudes en el sistema, filtrada según el estatus
+app.post("/RequestList", function (req, res) {
+  console.log(req.body);
+  Solicitud.findAll({
+    attributes: [
+      "id_S",
+      "fecha_Sol",
+      "fecha_Act",
+      "estatus",
+      "retroalimentacion",
+    ],
+    include: [
+      { model: Usuario, attributes: ["nombre_C"] },
+      { model: Tramite, attributes: ["nombre_T"] },
+    ],
+    where: { estatus: req.body.estatus },
+    order: [
+      ["fecha_Act", "ASC"],
+      ["fecha_Sol", "ASC"],
+    ],
+  }).then((consult) => {
+    res.send(consult);
+  });
+});
+
 //Inicializar el servidor
 app.listen(PORT, function () {
   console.log("http://localhost:" + PORT);
@@ -67,8 +110,9 @@ app.listen(PORT, function () {
   sequelize
     .authenticate()
     .then(() => {
-      sequelize.sync({ force: true });
-      console.log("Conectao");
+      sequelize.sync().then(() => {
+        console.log("Conectao");
+      });
     })
     .catch((error) => console.log("No conectao pq: ", error));
 });
