d0a8e11a5333fc4670e4449cea87da813f3f7dc2
Sergio Baldomero Beltran Santiago <sergiobaldomerobeltran@gmail.com>
2023-04-29 13:53:09 -0700

    Funciones para la carga de la información de los tramites, inicio del ruteamiento para mejor manejo del codigo.

---
+++ Routes/GestorTramites.js
@@ -0,0 +1,49 @@
+//Imports
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+
+//Models
+const Tramite = require("../Database/Models/Tramite");
+const Tramite_M = require("../Database/Models/Tramite_M");
+
+//CORS
+router.use(cors());
+
+//Middleware
+router.use(express.json({ limit: "10mb" }));
+router.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+//Envia todos los trámites existentes en formato de lista, para un botón de selección utilizado para la administración de los trámites
+router.get("/ObtenerListaTramites", function (req, res) {
+  Tramite.findAll({ attributes: ["id_Tramite", "nombre_Tramite"] })
+    .then((resultado) => {
+      res.send(resultado);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send({ Code: 0 });
+    });
+});
+
+//Envia toda la información y requisitos pertenecientes a un trámite en particular, para su posterior edición
+router.post("/ObtenerDatosTramite", function (req, res) {
+  Tramite.findAll({
+    attributes: ["id_Tramite", "nombre_Tramite"],
+    include: {
+      model: Tramite_M,
+    },
+    where: {
+      id_Tramite: req.body.ID_Tramite,
+    },
+  })
+    .then((resultado) => {
+      res.send(resultado);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send({ Code: 0 });
+    });
+});
+
+module.exports = router;
