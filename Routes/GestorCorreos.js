94cae9c5128d5b7c4c4565e974a749532e959d92
Sergio Baldomero Beltran Santiago <sergiobaldomerobeltran@gmail.com>
2023-05-26 15:51:47 -0700

    Routing para manejar correos, falta subir y borrar archivos, pero ya casi queda

--- Routes/GestorCorreos.js
+++ Routes/GestorCorreos.js
@@ -103,4 +103,140 @@ var transporte = nodemailer.createTransport({
   },
 });
 
+//Obtener la lista de los correos existentes
+router.get("/lista", async function (req, res, next) {
+  try {
+    //Determinamos el directorio para las plantillas de correos
+    const directorioCorreos = path.join(__dirname, "..", "/JSON");
+
+    //Intentamos acceder a la carpeta
+    fs.readdir(directorioCorreos, (error, archivos) => {
+      if (error) {
+        //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+        next(error);
+        return;
+      }
+
+      //Logramos acceder a la carpeta, ahora debemos acceder a cada archivo y obtener el titulo de cada uno
+      const correos = [];
+
+      archivos.forEach((archivo) => {
+        //Obtenemos la posible ruta del archivo.
+        const rutaPlantillaCorreo = path.join(directorioCorreos, archivo);
+
+        //Tratamos de leer el archivo
+        const plantillaCorreo = fs.readFileSync(rutaPlantillaCorreo);
+
+        //Ahora convertimos ese archivo a un formato JSON para mejor lectura dentro del programa
+        const correo = JSON.parse(plantillaCorreo);
+
+        //Añadimos cada correo posible al arreglo de correos.
+        correos.push(correo);
+      });
+
+      correos.length > 0 ? res.status(200).send(correos) : res.sendStatus(204);
+    });
+  } catch (error) {
+    //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+    next(error);
+  }
+});
+
+//Descargar documento asociado a la plantilla
+router.get("/documento", async function (req, res, next) {
+  try {
+    //Preparamos las rutas del directorio y archivo a descargar.
+    const rutaDocumentos = path.join(
+      __dirname,
+      "..",
+      "/Documentos/Correos/" + String(req.query.plantillaTitulo)
+    );
+
+    const rutaDocumentoPlantillaCorreo = path.join(
+      rutaDocumentos,
+      String(req.query.plantillaNombreArchivo)
+    );
+
+    //Preguntamos si el archivo existe
+    fs.access(rutaDocumentoPlantillaCorreo, fs.constants.F_OK, (erro) => {
+      erro
+        ? res.sendStatus(404)
+        : res.download(rutaDocumentoPlantillaCorreo, (err) => {
+            if (err) {
+              //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+              next(error);
+            }
+          });
+    });
+  } catch (error) {
+    //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+    next(error);
+  }
+});
+
+//Actualizar una de las plantillas de correo
+router.put("/actualizar", async function (req, res, next) {
+  try {
+    //Intentaremos abrir el archivo .json de la plantilla
+    const rutaDocumentoPlantilla = path.join(
+      __dirname,
+      "../JSON/",
+      String(req.body.nombreArchivoPlantilla) === "Correo de seguimiento"
+        ? "seguimiento.json"
+        : "inicio.json"
+    );
+
+    fs.readFile(rutaDocumentoPlantilla, "utf8", (error, informacion) => {
+      if (error) {
+        //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+        next(error);
+        return;
+      }
+
+      //Convertimos el archivo a un JSON manipulable
+      var plantillaCorreo = JSON.parse(informacion);
+
+      //Modificamos destinatario en caso de que haya cambios
+      plantillaCorreo.destinatario =
+        req.body.nuevoDestinatario !== ""
+          ? req.body.nuevoDestinatario
+          : plantillaCorreo.destinatario;
+
+      //Modificamos asunto en caso de que haya cambios
+      plantillaCorreo.asunto =
+        req.body.nuevoAsunto !== ""
+          ? req.body.nuevoAsunto
+          : plantillaCorreo.asunto;
+
+      //Modificamos cuerpo en caso de que haya cambios
+      plantillaCorreo.cuerpo =
+        req.body.nuevoCuerpo !== ""
+          ? req.body.nuevoCuerpo
+          : plantillaCorreo.cuerpo;
+
+      //Una vez terminados los cambios, reescribimos el archivo JSON
+      fs.writeFile(
+        rutaDocumentoPlantilla,
+        JSON.stringify(plantillaCorreo),
+        (error) => {
+          if (error) {
+            //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+            next(error);
+          }
+          res.sendStatus(200);
+        }
+      );
+    });
+  } catch (error) {
+    //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+    next(error);
+  }
+});
+
+//Añadir nuevos documentos a una de las plantillas de correo
+router.post("/subir", async function (req, res, next) {});
+
+//Eliminar un documento de una plantilla de correo
+router.get("/eliminar", async function (req, res, next) {});
+
 module.exports = router;
