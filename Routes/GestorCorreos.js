058d4843b4de2e105c8a88ff3a83e330fda63b91
Sergio Baldomero Beltran Santiago <sergiobaldomerobeltran@gmail.com>
2023-05-26 20:03:06 -0700

    Añadido funcion para actualizar las plantillas y documentos asociados

--- Routes/GestorCorreos.js
+++ Routes/GestorCorreos.js
@@ -16,7 +16,7 @@ router.use(express.static(__dirname));
 var storage = multer.diskStorage({
   destination: (req, file, callBack) => {
     //Vamos a determinar si los archivos van a la carpeta de solicitudes o correos
-    var carpeta = req.body.isSolicitud ? "Solicitudes" : "Correos";
+    var carpeta = req.body.isSolicitud === true ? "Solicitudes" : "Correos";
     //Aquí determinamos si la subcarpeta es la matricula del estudiante o la ID de la plantilla del correo.
     var subCarpeta = req.body.subCarpeta;
     //Checamos que el directorio dinamico exista, si no existe, se crea
@@ -234,9 +234,149 @@ router.put("/actualizar", async function (req, res, next) {
 });
 
 //Añadir nuevos documentos a una de las plantillas de correo
-router.post("/subir", async function (req, res, next) {});
+router.post("/subir", upload, async function (req, res, next) {
+  try {
+    if (!req.files) {
+      res.sendStatus(400);
+    } else {
+      //Abrimos el JSON de la plantilla
+      const rutaDocumentoPlantilla = path.join(
+        __dirname,
+        "../JSON/",
+        String(req.body.subCarpeta) === "Correo de seguimiento"
+          ? "seguimiento.json"
+          : "inicio.json"
+      );
+      //Intentamos abrirlo
+      fs.readFile(rutaDocumentoPlantilla, "utf8", (error, informacion) => {
+        if (error) {
+          //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+          next(error);
+          return;
+        }
+
+        //Convertimos el archivo a un JSON manipulable
+        var plantillaCorreo = JSON.parse(informacion);
+
+        //Obtenemos el arreglo de archivos ya existentes, o uno vacio, en caso de no existir
+        const archivosPlantilla =
+          plantillaCorreo.adjuntos.length > 0
+            ? plantillaCorreo.adjuntos.split(";")
+            : [];
+
+        //Iteramos los archivos de entrada, y añadimos su nombre al arreglo
+        for (var indice = 0; indice < req.files.length; indice++) {
+          archivosPlantilla.push(req.files[indice].filename);
+        }
+
+        //Convertimos el arreglo con los nuevos elementos en un string, separando elementos con un ;
+        const nuevosArchivosPlantilla = archivosPlantilla.join(";");
+
+        //Reemplazamos el atributo de adjuntos anterior por el nuevo
+        plantillaCorreo.adjuntos = nuevosArchivosPlantilla;
+
+        //Reescribimos el archivo de nuevo
+        fs.writeFile(
+          rutaDocumentoPlantilla,
+          JSON.stringify(plantillaCorreo),
+          (error) => {
+            if (error) {
+              //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+              next(error);
+            }
+            res.sendStatus(200);
+          }
+        );
+      });
+    }
+  } catch (error) {
+    //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+    next(error);
+  }
+});
 
 //Eliminar un documento de una plantilla de correo
-router.get("/eliminar", async function (req, res, next) {});
+router.get("/eliminar", async function (req, res, next) {
+  try {
+    //Abrimos el JSON de la plantilla
+    const rutaDocumentoPlantilla = path.join(
+      __dirname,
+      "../JSON/",
+      String(req.query.subCarpeta) === "Correo de seguimiento"
+        ? "seguimiento.json"
+        : "inicio.json"
+    );
+
+    //Intentamos abrirlo
+    fs.readFile(rutaDocumentoPlantilla, "utf8", (error, informacion) => {
+      if (error) {
+        //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+        next(error);
+        return;
+      }
+
+      //Convertimos el archivo a un JSON manipulable
+      var plantillaCorreo = JSON.parse(informacion);
+
+      //Obtenemos el arreglo de archivos ya existentes, o uno vacio, en caso de no existir
+      const archivosPlantilla =
+        plantillaCorreo.adjuntos.length > 0
+          ? plantillaCorreo.adjuntos.split(";")
+          : [];
+
+      //Eliminamos los archivos que ya no son deseados de el JSON seleccionado y del directorio
+      for (
+        var indice = 0;
+        indice < req.query.DocumentosEliminar.length;
+        indice++
+      ) {
+        //Esta parte elimina el archivo del atributo adjuntos del JSON seleccionado
+        const indiceArchivo = archivosPlantilla.indexOf(
+          req.query.DocumentosEliminar[indice]
+        );
+        archivosPlantilla.splice(indiceArchivo, 1);
+
+        //Esta parte borra el archivo en cuestion del directorio
+        fs.unlink(
+          path.join(
+            __dirname,
+            "../Documentos/Correos",
+            req.query.subCarpeta,
+            "/",
+            req.query.DocumentosEliminar[indice]
+          ),
+          (error) => {
+            if (error) {
+              //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+              next(error);
+            }
+          }
+        );
+      }
+
+      //Convertimos el arreglo con los nuevos elementos en un string, separando elementos con un ;
+      const nuevosArchivosPlantilla = archivosPlantilla.join(";");
+
+      //Reemplazamos el atributo de adjuntos anterior por el nuevo
+      plantillaCorreo.adjuntos = nuevosArchivosPlantilla;
+
+      //Reescribimos el archivo de nuevo
+      fs.writeFile(
+        rutaDocumentoPlantilla,
+        JSON.stringify(plantillaCorreo),
+        (error) => {
+          if (error) {
+            //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+            next(error);
+          }
+          res.sendStatus(200);
+        }
+      );
+    });
+  } catch (error) {
+    //Cualquier error del sistema, se envia un status 500, se crea un log dentro del servidor.
+    next(error);
+  }
+});
 
 module.exports = router;
